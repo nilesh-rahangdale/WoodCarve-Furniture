8bade5ef450a333621ba3f64712f1bef1cb239d2
nilesh-rahangdale <nileshrahangdale08@gmail.com>
2024-11-19 15:53:03 +0530

    Authentication Initialised

--- animated-login/index.js
+++ animated-login/index.js
@@ -1,12 +1,22 @@
+// const firebaseConfig = {
+//   apiKey: "AIzaSyDAAhJ8gEJqf_IrGXu3XCB29lOb7OT1yw8",
+//   authDomain: "kunal-59bf3.firebaseapp.com",
+//   databaseURL: "https://niyora-auth-default-rtdb.firebaseio.com",
+//   projectId: "kunal-59bf3",
+//   storageBucket: "kunal-59bf3.appspot.com",
+//   messagingSenderId: "371380172874",
+//   appId: "1:371380172874:web:9ecf1bb1c54c12f5800eb3",
+//   measurementId: "G-MJFB8S0PNZ"
+// };
 const firebaseConfig = {
-  apiKey: "AIzaSyDAAhJ8gEJqf_IrGXu3XCB29lOb7OT1yw8",
-  authDomain: "kunal-59bf3.firebaseapp.com",
-  databaseURL: "https://kunal-59bf3-default-rtdb.firebaseio.com",
-  projectId: "kunal-59bf3",
-  storageBucket: "kunal-59bf3.appspot.com",
-  messagingSenderId: "371380172874",
-  appId: "1:371380172874:web:9ecf1bb1c54c12f5800eb3",
-  measurementId: "G-MJFB8S0PNZ"
+  apiKey: "AIzaSyCRiFQ60U51xKM04A_ulgFHyAhRwNFd3V0",
+  authDomain: "niyora-auth.firebaseapp.com",
+  databaseURL: "https://niyora-auth-default-rtdb.firebaseio.com",
+  projectId: "niyora-auth",
+  storageBucket: "niyora-auth.firebasestorage.app",
+  messagingSenderId: "512446739296",
+  appId: "1:512446739296:web:e39036e796c2820bb5e405",
+  measurementId: "G-7EE5GV6XE3"
 };
 
 // Initialize Firebase
@@ -88,3 +98,30 @@ function validateEmail(email) {
 function validatePassword(password) {
   return password.length >= 6;
 }
+
+
+
+// // my auth
+// <script type="module">
+//   // Import the functions you need from the SDKs you need
+//   import { initializeApp } from "https://www.gstatic.com/firebasejs/11.0.2/firebase-app.js";
+//   import { getAnalytics } from "https://www.gstatic.com/firebasejs/11.0.2/firebase-analytics.js";
+//   // TODO: Add SDKs for Firebase products that you want to use
+//   // https://firebase.google.com/docs/web/setup#available-libraries
+
+//   // Your web app's Firebase configuration
+//   // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+//   const firebaseConfig = {
+//     apiKey: "AIzaSyCRiFQ60U51xKM04A_ulgFHyAhRwNFd3V0",
+//     authDomain: "niyora-auth.firebaseapp.com",
+//     projectId: "niyora-auth",
+//     storageBucket: "niyora-auth.firebasestorage.app",
+//     messagingSenderId: "512446739296",
+//     appId: "1:512446739296:web:e39036e796c2820bb5e405",
+//     measurementId: "G-7EE5GV6XE3"
+//   };
+
+//   // Initialize Firebase
+//   const app = initializeApp(firebaseConfig);
+//   const analytics = getAnalytics(app);
+// </script>
